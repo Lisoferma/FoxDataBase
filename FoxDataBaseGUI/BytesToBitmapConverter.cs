831cad5abd2171dc14269a4987aa38aa495a479c
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-18 23:28:15 +0900

    Add an image size parameter in the converter

--- FoxDataBaseGUI/BytesToBitmapConverter.cs
+++ FoxDataBaseGUI/BytesToBitmapConverter.cs
@@ -10,16 +10,35 @@ namespace FoxDataBaseGUI;
 /// </summary>
 public class BytesToBitmapConverter : IValueConverter
 {
-    public object? Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    /// <summary>
+    /// Конвертировать из массива байт в bitmap изображение.
+    /// </summary>
+    /// <param name="value">Данные изображения в массиве байт.</param>
+    /// <param name="targetType">Не используется.</param>
+    /// <param name="parameter">Выходной размер изображения в пикселях (int).</param>
+    /// <param name="culture">Не используется.</param>
+    /// <returns></returns>
+    public object? Convert(object value,
+                           Type targetType,
+                           object parameter,
+                           System.Globalization.CultureInfo culture)
     {
         if (value == null) return null;
 
         byte[] imageData = (byte[])value;
-        return GetBitmapImageFromBytes(imageData);
+        int decodePixelWidth = 200;
+
+        if (parameter != null)
+            decodePixelWidth = System.Convert.ToInt32(parameter);
+        
+        return GetBitmapImageFromBytes(imageData, decodePixelWidth);
     }
 
 
-    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    public object ConvertBack(object value,
+                              Type targetType,
+                              object parameter,
+                              System.Globalization.CultureInfo culture)
     {
         throw new NotImplementedException();
     }
@@ -29,27 +48,29 @@ public class BytesToBitmapConverter : IValueConverter
     /// Конвертировать из массива байт в bitmap изображение.
     /// </summary>
     /// <param name="imageData">Данные изображения.</param>
+    /// <param name="decodeWidth">Выходной размер изображения в пикселях.</param>
     /// <returns>Bitmap изображение.</returns>
     /// <exception cref="ArgumentException"></exception>
-    public static BitmapImage GetBitmapImageFromBytes(byte[] imageData)
+    public static BitmapImage GetBitmapImageFromBytes(byte[] imageData, int decodeWidth)
     {
         if (imageData.Length == 0)
             throw new ArgumentException("Image data cannot be empty");
+        if (decodeWidth <= 0)
+            throw new ArgumentException("Decode width cannot be <= 0");
 
         BitmapImage image = new();
-        using MemoryStream mem = new(imageData);
+        using MemoryStream memory = new(imageData);
 
-        mem.Position = 0;
+        memory.Position = 0;
         image.BeginInit();
         image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
         image.CacheOption = BitmapCacheOption.OnLoad;
         image.UriSource = null;
-        image.StreamSource = mem;
-        image.DecodePixelWidth = 200;
-        image.DecodePixelHeight = 200;
+        image.StreamSource = memory;
+        image.DecodePixelWidth = decodeWidth;
         image.EndInit();
-
         image.Freeze();
+
         return image;
     }
 }
